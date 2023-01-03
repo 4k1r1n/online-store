55d0de78f7dbd7997b1a3c7b1f921a0d9d2f2a36
4k1r1n <91277105+4k1r1n@users.noreply.github.com>
2023-01-03 09:27:18 +0300

    Fix/15 add products to the cart (#20)
    * feat: implement button state change when adding/removing a product in card
    
    * feat: save cart button state in product card on reload
    
    * fix: condition when cart is empty
    
    * feat: implement cart and button states when adding/removing a product in product-details
    
    Co-authored-by: Arina <akira657@mail.ru>

--- src/components/controller/main-page.ts
+++ src/components/controller/main-page.ts
@@ -1,5 +1,9 @@
 import getProduct from '../../pages/product-details';
 import { PATHS } from '../../constants/constants';
+import fidnDataById from '../model/find-data';
+import { Product } from '../../types/types';
+import { addProduct, removeProduct } from '../model/cart';
+import { cartCounter } from '../view/header/header';
 
 export function handleQuerySearch() {
   const obj = JSON.parse(JSON.stringify({ ...localStorage }));
@@ -91,4 +95,86 @@ export function handleLocalStorageRange(event: Event, key: string, query: string
 export function handleLocalStorageSearch(key: string, value: string) {
   setLocalStorage(value, key);
   if (!localStorage.getItem(key)) localStorage.removeItem(key);
+}
+
+export function handleCartClick(flag: boolean, img: HTMLElement, event: Event) {
+  if (event.target instanceof HTMLElement) {
+    const e = event.target;
+    const idProduct = img.getAttribute('data-id');
+    if (idProduct) {
+      const objProduct = fidnDataById(+idProduct) as Product;
+      if (!flag) {
+        flag = true;
+        e.classList.remove('btn_product-add');
+        e.classList.add('btn_product-remove');
+        addProduct(objProduct);
+      } else {
+        flag = false;
+        e.classList.add('btn_product-add');
+        e.classList.remove('btn_product-remove');
+        removeProduct(objProduct);
+      }
+      if (localStorage.getItem('cart')) {
+        cartCounter.textContent = JSON.parse(localStorage.cart).length;
+      }
+    }
+  }
+
+  return flag;
+}
+
+export function handleAddProductClick(flag: boolean, event: Event) {
+  if (event.target instanceof HTMLElement) {
+    const e = event.target;
+    const idProduct = window.location.pathname.split('')[window.location.pathname.split('').length - 1];
+    if (idProduct) {
+      const objProduct = fidnDataById(+idProduct) as Product;
+      if (!flag) {
+        flag = true;
+        addProduct(objProduct);
+        e.textContent = 'remove from cart';
+      } else {
+        flag = false;
+        removeProduct(objProduct);
+        e.textContent = 'add to cart';
+      }
+      if (localStorage.getItem('cart')) {
+        cartCounter.textContent = JSON.parse(localStorage.cart).length;
+      }
+    }
+  }
+  return flag;
+}
+
+export function setStateCardButtons(flag: boolean, btn: HTMLElement, img: HTMLElement) {
+  let cart: Product[] = [];
+  if (localStorage.getItem('cart')) cart = JSON.parse(localStorage.cart);
+  const idProduct = img.getAttribute('data-id');
+  if (idProduct) {
+    const objProduct = fidnDataById(+idProduct) as Product;
+    cart.forEach((product) => {
+      if (product.id === objProduct.id) {
+        flag = true;
+        btn.classList.remove('btn_product-add');
+        btn.classList.add('btn_product-remove');
+      }
+    });
+  }
+  return flag;
+}
+
+export function setStateProductBtn(flag: boolean, btn: HTMLElement) {
+  let cart: Product[] = [];
+  if (localStorage.getItem('cart')) cart = JSON.parse(localStorage.cart);
+  const idProduct = window.location.pathname.split('')[window.location.pathname.split('').length - 1];
+  if (idProduct) {
+    const objProduct = fidnDataById(+idProduct) as Product;
+    cart.forEach((product) => {
+      if (product.id === objProduct.id) {
+        flag = true;
+        btn.textContent = 'remove from cart';
+      }
+    });
+  }
+  return flag;
 }
