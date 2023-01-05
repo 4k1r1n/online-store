b034369f4d74b54a0fd89436dee3fd5cdab64f1b
4k1r1n <arinakire@gmail.com>
2023-01-05 23:46:34 +0300

    feat: change 404 page layout

--- src/pages/404.ts
+++ src/pages/404.ts
@@ -1,3 +1,6 @@
+import createElement from '../utils/create-element';
+
 export default function getError() {
-  return `<div>404</div>`;
+  const error = createElement('h2git a', 'error wrapper', '404 page no found');
+  return error;
 }
