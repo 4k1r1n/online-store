491564be500b47ad769d2bd0683b8c3871f05312
4k1r1n <arinakire@gmail.com>
2023-01-16 21:41:41 +0300

    feat: add input validation constants

--- src/components/controller/modal.ts
+++ src/components/controller/modal.ts
@@ -4,14 +4,14 @@ import { changeCreditCardLogo } from '../model/modal';
 export function handleValidateNumberCreditCard(e: KeyboardEvent) {
   const event = e.target;
   if (event instanceof HTMLInputElement) {
-    if (!validation.CREDIT_CARD_NUMBER.test(`${event.value}${e.key}`)) e.preventDefault();
+    if (!validation.CREDIT_CARD_NUMBER_INPUT.test(`${event.value}${e.key}`)) e.preventDefault();
     changeCreditCardLogo(event.value);
   }
 }
 
 export function handleValidateCvvCreditCard(e: KeyboardEvent) {
   const event = e.target;
-  if (event instanceof HTMLInputElement && !validation.CREDIT_CARD_CVV.test(`${event.value}${e.key}`)) {
+  if (event instanceof HTMLInputElement && !validation.CREDIT_CARD_CVV_INPUT.test(`${event.value}${e.key}`)) {
     e.preventDefault();
   }
 }
@@ -20,6 +20,6 @@ export function handleValidateValidCreditCard(e: KeyboardEvent) {
   const event = e.target;
   if (event instanceof HTMLInputElement) {
     if (event.value.length === 2) event.value += '/';
-    if (!validation.CREDIT_CARD_INPUT_VALID.test(`${event.value.split('/').join('')}${e.key}`)) e.preventDefault();
+    if (!validation.CREDIT_CARD_VALID_INPUT.test(`${event.value.split('/').join('')}${e.key}`)) e.preventDefault();
   }
 }
