2377d7e4620c8e08f27423def1124f9decbd25ed
matthewTheWizzard <mmdrumnew@gmail.com>
2023-01-29 18:35:36 +0400

    refactor: change condition in view

--- src/components/view/cards-view/cards-view.ts
+++ src/components/view/cards-view/cards-view.ts
@@ -2,7 +2,7 @@ export default function changeView() {
   const container = document.querySelector('.cards.layout-3-column') as HTMLElement;
   const btn = document.querySelector('.btn__view') as HTMLElement;
   const id = btn.getAttribute('data-id');
-  if (!!id !== false) {
+  if (id) {
     if (id === 'columns') {
       container.classList.add('cards_list');
     } else if (id === 'list') {
